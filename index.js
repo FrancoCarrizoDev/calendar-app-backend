4359a1d1904a6a24a07e263eb8883043e1176561
Franco <francoadrianc@gmail.com>
2021-07-10 00:17:20 -0300

    updated, finish proyect

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const express = require('express');
 const { dbConnection } = require('./database/config');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
+
 // Crear el servidor de express
 const app = express();
 
 // Base de datos
-dbConnection()
+dbConnection();
 
 // CORRS
 
-app.use(cors())
+app.use(cors());
 
 // DIrectorio publico
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Lectura y parseo del body
 
-app.use(express.json())
+app.use(express.json());
 
 // Rutas
 
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
+
 // TODO: auth CRUD: Eventos
-
-
 
 // Escuchar peticiones
 
 app.listen(process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
-})
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,41 @@
-
-
-
-
 // Obtener eventos
 // Tienen que estar validadas
-router.get('/')
+const { Router } = require('express');
+const router = Router();
+const { validateJWT } = require('../middlewares/validateJWT');
+const {
+    getEvents,
+    updateEvents,
+    addEvents,
+    deleteEvents
+} = require('../controllers/events');
+const { check } = require('express-validator');
+const { validateFields } = require('../middlewares/validateFields');
+const { isDate } = require('../helpers/isDate');
+
+router.use(validateJWT);
+
+router.get('/', getEvents);
+
+// Crear Evento
+
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de fin es obligatoria').custom(isDate),
+        validateFields
+    ],
+    addEvents
+);
+
+// Actualizar Evento
+
+router.put('/:id', updateEvents);
+
+// Borrar Evento
+
+router.delete('/:id', deleteEvents);
+
+module.exports = router;
